63fd07a2402e522205c6d4af83db83550c8715ed
crismiche1 <crisnat1905@gmailcom>
2024-09-04 11:38:47 -0300

    cambios en la conexion bd

--- src/database/config.js
+++ src/database/config.js
@@ -1,13 +1,11 @@
-require('dotenv').config()
+require('dotenv').config(); // Cargar las variables de entorno desde el archivo .env
 
-
-
-const PORT = process.env.PORT || 3000
-const DB_USER = process.env.DB_USER || 'root'
-const DB_PASSWORD = process.env.DB_PASSWORD || 'saionara123'
-const DB_HOST = process.env.DB_HOST || 'localhost'
-const DB_DATABASE = process.env.DB_DATABASE || 'gestionEscolar'
-const DB_PORT = process.env.DB_PORT || 3306
+const PORT = process.env.PORT || 3000;
+const DB_USER = process.env.MYSQLUSER || process.env.DB_USER || 'root';
+const DB_PASSWORD = process.env.MYSQLPASSWORD || process.env.DB_PASSWORD || 'saionara123';
+const DB_HOST = process.env.MYSQLHOST || process.env.DB_HOST || 'localhost';
+const DB_DATABASE = process.env.MYSQLDATABASE || process.env.DB_DATABASE || 'gestionEscolar';
+const DB_PORT = process.env.MYSQLPORT || process.env.DB_PORT || 3306;
 
 module.exports = {
     PORT,
@@ -16,4 +14,4 @@ module.exports = {
     DB_HOST,
     DB_DATABASE,
     DB_PORT
-}
+};
